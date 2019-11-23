d5aecf85ca2ccb45531f69cde0ae5f2e3bb6a729
Dishant <dishant.aress@gmail.com>
2019-11-23 19:32:21 +0530

    new class has been added

---
+++ src/simple/B.java
@@ -0,0 +1,12 @@
+package simple;
+
+public class B {	
+	
+	public static void main(String[] args) {
+		String s1 = "testing";
+		
+		for(int i=s1.length()-1;i>=0;i--){
+			System.out.print(s1.charAt(i));
+		}
+	}
+}
